3fa64ab79fe4ee597b96e5837c14948b1c856e68
Jim <jim@advancebass.com>
2026-01-03 22:34:14 -0800

    chore: update footer text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row mx-auto px-4 md:px-8">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                        Built for bass players, by bass players.
+                        For bass players, built by a bass player
                     </p>
                 </div>
                 <div className="flex items-center gap-4">
